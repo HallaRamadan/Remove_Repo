d1225a10a440f426649360957673efe5cedcc4e8
HallaRamadan <s193731@>
2022-10-23 13:15:42 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Halla!");
+        System.out.println("And my last name is Ramadan!");
+    }
+}
